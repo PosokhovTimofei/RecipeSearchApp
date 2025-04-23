f09fa4ee6cf6f240bd349d20d03956036116b6c6
Posokhov Timofei <new-ur@list.ru>
2025-04-23 14:44:54 +0300

    sign in screen

--- app/src/main/java/com/example/recipesearchapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesearchapp/MainActivity.kt
@@ -5,21 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -31,8 +44,10 @@ import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -43,8 +58,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RecipeSearchAppTheme {  // Тема приложения
-                Navigation()  // Навигация между экранами
+            RecipeSearchAppTheme {
+                Navigation()
             }
         }
     }
@@ -54,15 +69,15 @@ class MainActivity : ComponentActivity() {
 fun Navigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "login") {
+    NavHost(navController = navController, startDestination = "greeting") {
+        composable("greeting") { GreetingScreen(navController) }
         composable("login") { LoginScreen(navController) }
-//        composable("register") { RegisterScreen(navController) }
-//        composable("search") { RecipeSearchScreen() }
+        composable("search") { RecipeSearchScreen() }
     }
 }
 
 @Composable
-fun LoginScreen(navController: NavController) {
+fun GreetingScreen(navController: NavController) {
     val buttonModifier = Modifier
         .width(200.dp)
         .height(40.dp)
@@ -97,11 +112,11 @@ fun LoginScreen(navController: NavController) {
             "Поиск рецептов",
             style = MaterialTheme.typography.headlineLarge,
             modifier = Modifier
-                .padding(bottom = 100.dp) // Только этот текст сдвигается вниз
-                .offset(y = (-50).dp)     // Дополнительное смещение вверх/вниз
+                .padding(bottom = 100.dp)
+                .offset(y = (-50).dp)
         )
 
-        Button(onClick = { navController.navigate("search") },
+        Button(onClick = { navController.navigate("login") },
             modifier = buttonModifier
                 .shadow(4.dp, shape = RoundedCornerShape(12.dp))
                 .clip(RoundedCornerShape(12.dp)),
@@ -114,7 +129,7 @@ fun LoginScreen(navController: NavController) {
         Spacer(modifier = Modifier.height(12.dp))
 
         Button(
-            onClick = { navController.navigate("register") },
+            onClick = { navController.navigate("search") },
             modifier = buttonModifier
                 .shadow(4.dp, shape = RoundedCornerShape(12.dp))
                 .clip(RoundedCornerShape(12.dp)),
@@ -124,4 +139,129 @@ fun LoginScreen(navController: NavController) {
         }
     }
 }
+}
+
+@Composable
+fun LoginScreen(navController: NavController) {
+
+    val buttonModifier = Modifier
+        .width(200.dp)
+        .height(40.dp)
+
+    val loginColors = ButtonDefaults.buttonColors(
+        containerColor = Color(0xFF16b88c),
+        contentColor = Color(0xfff5e5e9)
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Фон
+        Image(
+            painter = painterResource(id = R.drawable.background),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        IconButton(
+            onClick = { navController.navigate("greeting") },
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                contentDescription = "Назад",
+                tint = Color.DarkGray
+            )
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 120.dp), // отступ от верха
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Вход",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.DarkGray,
+                modifier = Modifier.padding(bottom = 32.dp)
+            )
+
+            // Username
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Username or Email") },
+                leadingIcon = {
+                    Icon(Icons.Default.Person, contentDescription = null)
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Password
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Password") },
+                leadingIcon = {
+                    Icon(Icons.Default.Lock, contentDescription = null)
+                },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            // Remember + Forgot
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(checked = false, onCheckedChange = {})
+                    Text("Запомнить меня", fontSize = 14.sp)
+                }
+
+                Text(
+                    text = "Забыли пароль?",
+                    color = Color(0xFF1A8F84),
+                    fontSize = 14.sp,
+                    modifier = Modifier.clickable { /* обработка */ }
+                )
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            // Sign In Button
+            Button(
+                onClick = { navController.navigate("home") },
+                modifier = buttonModifier
+                    .shadow(4.dp, shape = RoundedCornerShape(12.dp))
+                    .clip(RoundedCornerShape(12.dp)),
+                colors = loginColors
+            ) {
+                Text("Войти", color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(36.dp))
+        }
     }
+}
+
+@Composable
+fun RecipeSearchScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("Экран поиска рецептов", style = MaterialTheme.typography.headlineMedium)
+    }
+}
+
+
